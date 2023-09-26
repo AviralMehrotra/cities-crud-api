e49e30ffc3156004639119e3391b80bec96a0006
Aviral Mehrotra <73425338+AviralMehrotra@users.noreply.github.com>
2023-09-26 07:11:40 +0530

    changed to json-server

--- server.js
+++ server.js
@@ -1,84 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const fs = require("fs"); // Import the File System module
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-const dbFilePath = "db.json"; // Define the path to your JSON data file
-
-// Function to read data from the JSON file
-function readDataFromFile() {
-  try {
-    const data = fs.readFileSync(dbFilePath, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    console.error("Error reading data from file:", error);
-    return [];
-  }
-}
-
-// Function to write data to the JSON file
-function writeDataToFile(data) {
-  try {
-    fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2), "utf8");
-  } catch (error) {
-    console.error("Error writing data to file:", error);
-  }
-}
-
-// Define CRUD routes
-// 1. Create (POST)
-app.post("/cities", (req, res) => {
-  const newItem = req.body;
-  const data = readDataFromFile(); // Read the data from the file
-  newItem.id = Date.now(); // Generate a unique ID (you can use a more robust method)
-  data.push(newItem);
-  writeDataToFile(data); // Write the updated data back to the file
-  res.json(newItem);
-});
-
-// 2. Read (GET)
-app.get("/cities", (req, res) => {
-  const data = readDataFromFile(); // Read the data from the file
-  res.json(data);
-});
-
-// 3. Update (PUT)
-app.put("/cities/id", (req, res) => {
-  const itemId = parseInt(req.params.id);
-  const updatedItem = req.body;
-  let data = readDataFromFile(); // Read the data from the file
-  data = data.map((item) => (item.id === itemId ? updatedItem : item));
-  writeDataToFile(data); // Write the updated data back to the file
-  res.json(updatedItem);
-});
-
-// 4. Delete (DELETE)
-app.delete("/cities/id", (req, res) => {
-  const itemId = parseInt(req.params.id);
-  let data = readDataFromFile(); // Read the data from the file
-  data = data.filter((item) => item.id !== itemId);
-  writeDataToFile(data); // Write the updated data back to the file
-  res.json({ message: "Item deleted" });
-});
-// 5. Read a single item by ID (GET)
-app.get("/cities/id", (req, res) => {
-  const itemId = parseInt(req.params.id);
-  const data = readDataFromFile(); // Read the data from the file
-  const item = data.find((item) => item.id === itemId);
-
-  if (!item) {
-    res.status(404).json({ error: "Item not found" });
-  } else {
-    res.json(item);
-  }
-});
-
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
